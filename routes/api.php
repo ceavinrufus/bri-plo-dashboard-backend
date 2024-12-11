5a9888410cdafcd732645d8f7960d05c01d4af85
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-12-11 10:18:06 +0700

    feat: route for fetch all users

--- routes/api.php
+++ routes/api.php
@@ -7,12 +7,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PengadaanController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\UserController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::get('/user', [UserController::class, 'index']);
+Route::get('/users', [UserController::class, 'index']);
 
 // Pengadaan
 Route::get('/pengadaan/departemen/{pengadaan:departemen}', [PengadaanController::class, 'index']);
